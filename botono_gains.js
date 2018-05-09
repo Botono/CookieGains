3f217e2fd257ad7c33ab2eb460146be70551b4ea
Aaron Murray <aaron@aaron-murray.net>
2018-05-09 15:51:03 -0700

    Convert to closure and for run on click

--- botono_gains.js
+++ botono_gains.js
@@ -1,24 +1,24 @@
-var botono_Interval;
-var botono_bigCookie = document.getElementById('bigCookie');
-function botono_gains() {
+(function () {
+	var botono_Interval;
+	var botono_bigCookie = document.getElementById('bigCookie');
 	var cursorAmount = Game.Objects['Cursor'].amount;
+	
+	function botono_startClicking() {
+		botono_Interval = setInterval(botono_click, 1);
+		setTimeout(botono_stopClicking, 10000);
+	}
+
+	function botono_stopClicking() {
+		clearInterval(botono_Interval);
+	}
+
+	function botono_click() {
+		botono_bigCookie.click();
+	}
 
 	Game.Objects['Cursor'].sell(cursorAmount); // Sell all cursors
 
 	botono_startClicking();
 
 	Game.Objects['Cursor'].buy(cursorAmount); // Buy all cursors back
-}
-
-function botono_startClicking() {
-	botono_Interval = setInterval(botono_click, 1);
-	setTimeout(botono_stopClicking, 10000);
-}
-
-function botono_stopClicking() {
-	clearInterval(botono_Interval);
-}
-
-function botono_click() {
-	botono_bigCookie.click();
-}
+})();
