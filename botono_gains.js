f77a1337d2d89750980eeae8f4d557546418ed48
Aaron Murray <aaron@aaron-murray.net>
2018-05-19 21:59:59 -0700

    Fixed multiple click interval stacking bug

--- botono_gains.js
+++ botono_gains.js
@@ -1,11 +1,11 @@
 (function () {
-	var botono_Interval;
+	var botono_Interval = [];
 	var botono_bigCookie = document.getElementById('bigCookie');
 	var buttonContainer = document.createElement('div');
 	var sellAndClickButton = document.createElement('button');
 	var justClickButton = document.createElement('button');
 	var cursorAmount = Game.Objects['Cursor'].amount;
-	
+
 	buttonContainer.style = "position: relative; top: 120px; left: 5px; width: 246px;";
 	sellAndClickButton.innerHTML = 'Sell';
 	justClickButton.innerHTML = 'Click';
@@ -13,7 +13,7 @@
 	sellAndClickButton.style = "float: left;";
 	justClickButton.style = "float: right";
 	justClickButton.onclick = justClick;
-	
+
 	buttonContainer.appendChild(sellAndClickButton);
 	buttonContainer.appendChild(justClickButton);
 	botono_bigCookie.appendChild(buttonContainer);
@@ -29,18 +29,23 @@
 
 	function sellAndClick_worker() {
 		Game.Objects['Cursor'].sell(cursorAmount); // Sell all cursors
-		botono_Interval = setInterval(botono_click, 1);
+		botono_Interval.push(setInterval(botono_click, 1));
 		setTimeout(botono_stopClicking, 10000);
 		Game.Objects['Cursor'].buy(cursorAmount); // Buy all cursors back
 	}
 
 	function justClick_worker() {
-		botono_Interval = setInterval(botono_click, 1);
-		setTimeout(botono_stopClicking, 10000);
+		botono_Interval.push(setInterval(botono_click, 1));
+		setTimeout(botono_stopClicking, 2000);
 	}
 
 	function botono_stopClicking() {
-		clearInterval(botono_Interval);
+		if (botono_Interval.length > 0) {
+			botono_Interval = botono_Interval.filter(function (interval) {
+				clearInterval(interval);
+				return false;
+			});
+		}
 	}
 
 	function botono_click() {
